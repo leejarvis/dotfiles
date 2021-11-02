6f9432482f80b22e307a606e8e26214b442ef144
Lee Jarvis <leejarvis@fastmail.com>
2021-11-02 21:37:09 +0000

    misc changes

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,9 +1,12 @@
-if [[ -a  /usr/local/opt/chruby/share/chruby/chruby.sh ]]; then
-  source /usr/local/opt/chruby/share/chruby/chruby.sh
-  chruby 2.6.5
-fi
-
-if [[ -a /opt/homebrew/opt/chruby/share/chruby/chruby.sh ]]; then
-  source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
-  chruby 2.6.5
+# Only load /opt/homebrew chruby version on M1
+if [[ $OSTYPE == darwin* && $CPUTYPE == arm64 ]]; then
+  if [[ -a /opt/homebrew/opt/chruby/share/chruby/chruby.sh ]]; then
+    source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+    chruby 2.6.5
+  fi
+else
+  if [[ -a  /usr/local/opt/chruby/share/chruby/chruby.sh ]]; then
+    source /usr/local/opt/chruby/share/chruby/chruby.sh
+    chruby 2.6.5
+  fi
 fi
--- zshrc
+++ zshrc
@@ -55,8 +55,6 @@ if [[ $OSTYPE == darwin* && $CPUTYPE == arm64 ]]; then
   export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}"
   export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:"
   export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}"
+
+  . /usr/local/opt/asdf/libexec/asdf.sh
 fi
-
-if which spark-ansible > /dev/null; then source /etc/ansible/scripts/spark-autocomplete.sh; fi
-
-. /usr/local/opt/asdf/libexec/asdf.sh
